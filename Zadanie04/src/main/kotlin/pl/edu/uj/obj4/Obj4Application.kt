3d623846ed56ca3ce21ec8d33393642b36b38f7c
Michał Czyż <czyzu1710@gmail.com>
2021-04-26 00:31:08 +0200

    Created exercise 4

---
+++ Zadanie04/src/main/kotlin/pl/edu/uj/obj4/Obj4Application.kt
@@ -0,0 +1,11 @@
+package pl.edu.uj.obj4
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Obj4Application
+
+fun main(args: Array<String>) {
+    runApplication<Obj4Application>(*args)
+}

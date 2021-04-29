8f44f9faad6a66fe03f9378ee10159623dc5f3e7
Michał Czyż <czyzu1710@gmail.com>
2021-04-29 17:00:10 +0200

    new branch

---
+++ Zadanie04/src/test/kotlin/pl/edu/uj/obj4/Obj4ApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.edu.uj.obj4
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Obj4ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
